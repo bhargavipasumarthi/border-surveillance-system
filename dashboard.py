46892a8c69d717e52e35d9c8e5ba62a049d4d835
Bhargavi Pasumarthi <bhargavipasumarthi2007@gmail.com>
2026-06-13 02:11:07 +0530

    Create events table automatically

--- dashboard.py
+++ dashboard.py
@@ -106,6 +106,20 @@ st.sidebar.info(
 # Connect to database
 try:
     conn = sqlite3.connect("events.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        tracker_id INTEGER,
+        event TEXT,
+        threat_level TEXT
+)
+""")
+    
+conn.commit()
+df = pd.read_sql("SELECT * FROM events", conn)
 
     df = pd.read_sql_query(
         "SELECT * FROM events",
